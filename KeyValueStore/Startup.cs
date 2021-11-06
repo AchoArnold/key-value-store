30ecba1e79a264003895f65ea749b182367901d5
Acho Arnold <arnold@ndolestudio.com>
2021-11-06 16:28:56 +0200

    Add kubernates deployment

--- KeyValueStore/Startup.cs
+++ KeyValueStore/Startup.cs
@@ -66,14 +66,15 @@ namespace KeyValueStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Key Value Store API v1");
-                    c.RoutePrefix = string.Empty;
-                    c.DocumentTitle = "Key Value store";
-                });
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Key Value Store API v1");
+                c.RoutePrefix = string.Empty;
+                c.DocumentTitle = "Key Value store";
+            });
 
             app.UseHttpsRedirection();
 
